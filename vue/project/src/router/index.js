d77e9dc2b8ee704be36bae73c76a4b29947c55dc
m-ur-phy <ooiillppaaiinntt@gmail.com>
2022-12-08 16:30:40 +0900

    vue test

--- vue/project/src/router/index.js
+++ vue/project/src/router/index.js
@@ -1,29 +1,17 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Router from 'vue-router'
+//아래에 연결할 서브페이지(콤퍼넌드)를 import
+import main_page from '../main_page.vue';
+import sub_page from '../sub_page.vue';
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
-const router = new VueRouter({
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    { path: '/', component: main_page},
+    { path: '/main', component: main_page},
+    { path: '/sub', component: sub_page}
+  ]
 })
-
-export default router
