b9d094dcc739feeb27115ed75979e4968b5b7304
m-ur-phy <ooiillppaaiinntt@gmail.com>
2022-12-09 13:11:59 +0900

    vuetify swiper fontawesome setting

---
+++ vue/project/src/components/MainContent.vue
@@ -0,0 +1,41 @@
+<!-- 메인 페이지 마크업 -->
+<template lang="html">
+<div>
+  <MainVisual />
+    <article class="">
+      <h2>메인 페이지 입니다.</h2>
+      <ul class="d-flex">
+        <li><i class="fa-regular fa-futbol"></i></li>
+        <li><i class="fa-regular fa-face-grin-tongue-squint"></i></li>
+        <li><i class="fa-regular fa-circle-check"></i></li>
+        <li><i class="fa-solid fa-earth-asia"></i></li>
+        
+        
+        
+       
+
+      </ul>
+    </article>
+    </div>
+  </template>
+  
+  <script>
+  import MainVisual from './MainVisual.vue'
+
+
+  export default {
+    data(){
+    return{
+
+    }
+  },
+  components:{
+      MainVisual
+  }
+  }
+  </script>
+  
+  <style>
+    .box1{width: 90%; margin: 10px 5%; padding: 20px; box-sizing: border-box;
+     background: crimson; color: #fff;}
+  </style>
---
+++ vue/project/src/components/Sub1Content.vue
@@ -0,0 +1,18 @@
+<!-- 서브 페이지 마크업 -->
+<template lang="html">
+    <div class="box2">
+      <h2>서브 페이지1 입니다.</h2>
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+   
+  }
+  </script>
+  
+  <style>
+    .box2{width: 90%; margin: 10px 5%; padding: 20px; box-sizing: border-box;
+     background:darkseagreen; color: #333;}
+  </style>
